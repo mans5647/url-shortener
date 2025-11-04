671a4e343e7535b3c481bfc6a58741ac9a3e71fc
mansur <mansurgalimov161@gmail.com>
2025-11-04 20:48:35 +0300

    Uncomment tests and fix some API

--- tests/mongo_test.go
+++ tests/mongo_test.go
@@ -1,138 +1,136 @@
 package tests
 
-// import (
-// 	"context"
-// 	"testing"
-// 	"url-shortener/models/url"
-// 	"url-shortener/service"
-// 	"go.mongodb.org/mongo-driver/v2/bson"
-// 	"go.mongodb.org/mongo-driver/v2/mongo/readpref"
-// )
+import (
+	"context"
+	"testing"
+	"url-shortener/models/url"
+	"url-shortener/service"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo/readpref"
+)
 
-// func TestConnection(t * testing.T) {
+func TestConnection(t * testing.T) {
 
-// 	ctx := context.Background()
+	ctx := context.Background()
 
-// 	client, err := service.NewMongoConnection("localhost", 27017)
-// 	if err != nil {
-// 		t.Error()
-// 	}
+	client, err := service.NewMongoConnection("localhost", 27017)
+	if err != nil {
+		t.Error()
+	}
 
-// 	defer func() {
-//     if err := client.Disconnect(ctx); err != nil {
-//         panic(err)
-//     }
-// 	}()
+	defer func() {
+    if err := client.Disconnect(ctx); err != nil {
+        panic(err)
+    }
+	}()
 
-// 	if client.Ping(ctx, readpref.Primary()) != nil {
-// 		t.Error()
-// 	}
+	if client.Ping(ctx, readpref.Primary()) != nil {
+		t.Error()
+	}
 
-// }
+}
 
-// func TestInsertOne(t * testing.T) {
-// 	ctx := context.Background()
+func TestInsertOne(t * testing.T) {
+	ctx := context.Background()
 
-// 	oldUrl := url.PlainUrl{
-// 		Url: "https://www.google.com",
-// 	}
-
-// 	shortener := url.SequenceShortener{}
-
-// 	newUrl, err := shortener.Shorten(ctx, url.DefaultCutSize, &oldUrl)
-
-// 	if err != nil {
-// 		t.Error()
-// 	}
-
-// 	client, err := service.NewMongoConnection("localhost",27017)
-
-// 	if err != nil {
-// 		t.Errorf("failed to connect to mongo!")
-// 	}
-
-// 	defer func() {
-//     if err := client.Disconnect(ctx); err != nil {
-//         panic(err)
-//     }
-// 	}()
-
-// 	if client.Ping(ctx, readpref.Primary()) != nil {
-// 		t.Error()
-// 	}
-
-// 	db := client.Database("url_shortener")
-
-// 	coll := db.Collection("urls")
+	oldUrl := url.PlainUrl{
+		Url: "https://www.google.com",
+	}
 
 
-// 	_, err = coll.InsertOne(ctx, newUrl)
+	newUrl, err := url.Shorten(ctx, 1, url.DefaultCutSize, &oldUrl)
 
-// 	if err != nil {
-// 		t.Errorf("insert one error!")
-// 	}
+	if err != nil {
+		t.Error()
+	}
 
-// 	_, err = coll.DeleteMany(ctx,bson.D{{"_id", newUrl.Id},})
+	client, err := service.NewMongoConnection("localhost",27017)
 
-// 	if err != nil {
-// 		t.Error("error clearing entries")
-// 	}
+	if err != nil {
+		t.Errorf("failed to connect to mongo!")
+	}
 
-// }
+	defer func() {
+    if err := client.Disconnect(ctx); err != nil {
+        panic(err)
+    }
+	}()
 
-// func TestInsertMany(t * testing.T) {
+	if client.Ping(ctx, readpref.Primary()) != nil {
+		t.Error()
+	}
 
-// 	ctx := context.Background()
+	db := client.Database("url_shortener")
 
-// 	oldUrls := []url.PlainUrl{
-// 		{Url: "https://www.google.com"},
-// 		{Url: "https://yandex.ru"},
-// 		{Url: "https://https://www.mongodb.com/docs/drivers/go/"},
-// 	}
+	coll := db.Collection("urls")
 
-// 	shortener := url.SequenceShortener{}
 
-// 	client, err := service.NewMongoConnection("localhost",27017)
+	_, err = coll.InsertOne(ctx, newUrl)
 
-// 	if err != nil {
-// 		t.Errorf("failed to connect to mongo!")
-// 	}
+	if err != nil {
+		t.Errorf("insert one error!")
+	}
 
-// 	defer func() {
-//     if err := client.Disconnect(ctx); err != nil {
-//         panic(err)
-//     }
-// 	}()
+	_, err = coll.DeleteMany(ctx,bson.D{})
 
-// 	if client.Ping(ctx, readpref.Primary()) != nil {
-// 		t.Error()
-// 	}
+	if err != nil {
+		t.Error("error clearing entries")
+	}
 
-// 	db := client.Database("url_shortener")
+}
 
-// 	coll := db.Collection("urls")
+func TestInsertMany(t * testing.T) {
 
-// 	newUrls := []*url.ShortUrl{}
-// 	for _, old := range oldUrls {
-// 		newUrl, err := shortener.Shorten(ctx, url.DefaultCutSize, &old)
+	ctx := context.Background()
 
-// 		if err != nil {
-// 			t.Errorf("error in shortening")
-// 		}
+	oldUrls := []url.PlainUrl{
+		{Url: "https://www.google.com"},
+		{Url: "https://yandex.ru"},
+		{Url: "https://https://www.mongodb.com/docs/drivers/go/"},
+	}
 
-// 		newUrls = append(newUrls, newUrl)
-// 	}
 
-// 	_, err = coll.InsertMany(ctx, newUrls)
+	client, err := service.NewMongoConnection("localhost",27017)
 
-// 	if err != nil {
-// 		t.Errorf("InsertMany() error")
-// 	}
+	if err != nil {
+		t.Errorf("failed to connect to mongo!")
+	}
 
-// 	_, err = coll.DeleteMany(ctx, bson.D{})
+	defer func() {
+    if err := client.Disconnect(ctx); err != nil {
+        panic(err)
+    }
+	}()
 
-// 	if err != nil {
-// 		t.Errorf("error clearing entries")
-// 	}
+	if client.Ping(ctx, readpref.Primary()) != nil {
+		t.Error()
+	}
 
-// }
+	db := client.Database("url_shortener")
+
+	coll := db.Collection("urls")
+
+	newUrls := []*url.ShortUrl{}
+	for i, old := range oldUrls {
+		newUrl, err := url.Shorten(ctx, i ,url.DefaultCutSize, &old)
+
+		if err != nil {
+			t.Errorf("error in shortening")
+		}
+
+		newUrls = append(newUrls, newUrl)
+	}
+
+	_, err = coll.InsertMany(ctx, newUrls)
+
+	if err != nil {
+		t.Errorf("InsertMany() error")
+	}
+
+	_, err = coll.DeleteMany(ctx, bson.D{})
+
+	if err != nil {
+		t.Errorf("error clearing entries")
+	}
+
+}
--- tests/url_test.go
+++ tests/url_test.go
@@ -1,40 +1,39 @@
 package tests
 
-// import (
-// 	"context"
-// 	"testing"
-// 	"url-shortener/models/url"
-// )
+import (
+	"context"
+	"testing"
+	"url-shortener/models/url"
+)
 
-// func allStrinsEqual(a []string) bool {
-// 	for i := 1; i < len(a); i++ {
-//         if a[i] != a[0] {
-//             return false
-//         }
-//     }
-//     return true
-// }
+func allStrinsEqual(a []string) bool {
+	for i := 1; i < len(a); i++ {
+        if a[i] != a[0] {
+            return false
+        }
+    }
+    return true
+}
 
-// func Test_SequenceShortening_equal_urls(t * testing.T) {
 
-// 	ss := url.SequenceShortener{}
+func TestShortAllCodesUnique(t * testing.T) {
 
-// 	codes := []string{}
+	codes := []string{}
 
-// 	plainUrl := &url.PlainUrl{
-// 		Url: "https://www.google.com",
-// 	}
+	plainUrl := &url.PlainUrl{
+		Url: "https://www.google.com",
+	}
 
-// 	iterCount := 10000
+	iterCount := 1 << 16
 
-// 	ctx := context.Background()
-// 	for i := 0; i < iterCount; i++ {
-// 		value, _ := ss.Shorten(ctx, url.DefaultCutSize, plainUrl)
-// 		codes = append(codes, value.ShortCode)
-// 	}
+	ctx := context.Background()
+	for i := 0; i < iterCount; i++ {
+		value, _ := url.Shorten(ctx, i, url.DefaultCutSize, plainUrl)
+		codes = append(codes, value.ShortCode)
+	}
 
-// 	if allStrinsEqual(codes) == true {
-// 		t.Errorf("not all unique: %v", codes)
-// 	}
+	if allStrinsEqual(codes) {
+		t.Errorf("allStringsEqual(): all codes must be unique, but aren't")
+	}
 
-// }
+}
